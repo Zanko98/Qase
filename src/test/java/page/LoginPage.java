bda23cfff481da7e839427d8e0850bc7600887bb
Marina <87647436+Zanko98@users.noreply.github.com>
2022-04-19 10:59:55 +0300

    Update LoginPage.java

--- src/test/java/page/LoginPage.java
+++ src/test/java/page/LoginPage.java
@@ -2,7 +2,6 @@ package page;
 
 import com.codeborne.selenide.Condition;
 import org.testng.annotations.Test;
-
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
