a0a58bafc24741da60c7022f97310623bb9edab2
m.zanko <Zanko98@mail.ru>
2022-04-26 12:09:05 +0300

    correct ProjectList

--- src/test/java/page/ProjectList.java
+++ src/test/java/page/ProjectList.java
@@ -1,6 +1,10 @@
 package page;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.ElementsCollection;
+import org.openqa.selenium.By;
+import static com.codeborne.selenide.Selenide.screenshot;
+
 import static com.codeborne.selenide.Selenide.*;
 
 public class ProjectList extends BasePage {
@@ -15,9 +19,21 @@ public class ProjectList extends BasePage {
 
     public String getProjectName(String index) {
         ElementsCollection element = $$(".defect-title");
-        if(index.equals("last"))
+        if(index.equals("last")){
+            if ($(".mt-5").text().isEmpty())
+                goingThePage("last");
             return element.last().getText();
+        }
         else
             return element.get(Integer.parseInt(index)+1).getText();
+    }
+
+    public void goingThePage(String number) {
+        if(number.equals("last")){
+            $(By.xpath("//ul[@class = 'style_pages-sM-RA']/li[last()-1]/button")).click();
+            $(".project-name").screenshot();
+        }
+        else
+            $(By.xpath(String.format("//button[text()='%s']", Integer.parseInt(number)+1))).click();
     }
 }
